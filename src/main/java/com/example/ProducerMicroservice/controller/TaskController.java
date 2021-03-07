680d86ed835fe4ff7ba59287051ae2ab6280f144
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-03-07 13:03:28 +0100

    Task controller

--- src/main/java/com/example/ProducerMicroservice/controller/TaskController.java
+++ src/main/java/com/example/ProducerMicroservice/controller/TaskController.java
@@ -3,8 +3,10 @@ package com.example.ProducerMicroservice.controller;
 import com.example.ProducerMicroservice.model.dto.TaskDTO;
 import com.example.ProducerMicroservice.service.TaskService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class TaskController {
     @ApiOperation("Returns all tasks from database")
     public List<TaskDTO> findAll() {
         return taskService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TaskDTO findById(@ApiParam(value = "Id of a task which we want to find") @PathVariable int id) {
+        return taskService.findById(id);
     }
 
 
