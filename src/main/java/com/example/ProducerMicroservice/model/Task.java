9b0e8c398e62c4f87e76726fc0f5a62def6897f9
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2021-03-07 10:01:17 +0100

    task

--- src/main/java/com/example/ProducerMicroservice/model/Task.java
+++ src/main/java/com/example/ProducerMicroservice/model/Task.java
@@ -1,4 +1,32 @@
 package com.example.ProducerMicroservice.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Table
+@Entity
+@NoArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    @Column
+    @NotNull
+    private String description;
+    @Column
+    private boolean completed;
+    @Column
+    @Min(0)
+    @Max(100)
+    private Integer completionPercentage;
+    @ManyToOne(fetch = FetchType.LAZY)
+    public User user;
 }
